c26d194bfc710481e7e34929f42295dede12feaf
Felipe de Araujo <oifelipearaujo@gmail.com>
2017-04-13 17:19:59 -0300

    Add home page

---
+++ app/home/home.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('testaller.home', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {
+    templateUrl: 'home/home.html',
+    controller: 'HomeCtrl'
+  });
+}])
+
+.controller('HomeCtrl', ['$scope', function($scope) {
+  $scope.status = {
+    isopen: false
+  };
+
+  $scope.isNavCollapsed = true;
+
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+  };
+}]);
