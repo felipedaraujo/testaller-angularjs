70f422f570b722e4b38bb43d813257016737e3e3
Felipe de Araujo <oifelipearaujo@gmail.com>
2017-04-17 17:51:25 -0300

    Adds signup & signin methods

--- app/signin/signin.js
+++ app/signin/signin.js
@@ -9,8 +9,26 @@ angular.module('testaller.signin', ['ngRoute'])
   });
 }])
 
-.controller('SigninCtrl', ['$scope', '$location', function($scope, $location) {
-  $scope.signin = function($event) {
-    $location.path('/home');
+.controller('SigninCtrl', ['$scope', '$http', '$location', '$localStorage', 'API_URL',
+                          function($scope, $http, $location, $localStorage, API_URL) {
+
+  $scope.loading = false;
+  $scope.error   = {};
+
+  $scope.signin = function(email, password) {
+    $scope.loading = true;
+
+    $http.post(API_URL + '/auth/signin', {
+      email: email,
+      password: password
+    }).success(function(response) {
+      if(reponse.auth_token) {
+        $localStorage.auth_token = response.auth_token;
+        $location.path('/home');
+      } else {
+        $scope.error.message = {};
+        $scope.loading = false;
+      }
+    });
   };
 }]);
--- app/users/users.js
+++ app/users/users.js
@@ -14,9 +14,27 @@ angular.module('testaller.users', ['ngRoute'])
     });
 }])
 
-.controller('UsersNewCtrl', ['$scope', function($scope) {
-  $scope.signup = function($event) {
-    $location.path('/home');
+.controller('UsersNewCtrl', ['$scope', '$http', '$location', '$localStorage', 'API_URL',
+                            function($scope, $http, $location, $localStorage, API_URL) {
+  $scope.loading = false;
+  $scope.error   = {};
+
+  $scope.signup = function(email, password, password_confirmation) {
+    $scope.loading = true;
+
+    $http.post(API_URL + '/signup', {
+      email: email,
+      password: password,
+      password_confirmation: password_confirmation
+    }).success(function(response) {
+      if(reponse.auth_token) {
+        $localStorage.auth_token = response.auth_token;
+        $location.path('/home');
+      } else {
+        $scope.error.message = {};
+        $scope.loading = false;
+      }
+    });
   };
 }])
 
