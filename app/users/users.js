e09bfed18f13ad893464c33fdd17331c4ff48091
Felipe de Araujo <oifelipearaujo@gmail.com>
2017-04-18 01:37:32 -0300

    Remove signup endpoint

--- app/users/users.js
+++ app/users/users.js
@@ -24,7 +24,7 @@ angular.module('testaller.users', ['ngRoute'])
     $scope.error   = {};
 
     $http({
-      url: API_URL + '/signup',
+      url: API_URL + '/users',
       skipAuthorization: true,
       method: 'POST',
       data: {
