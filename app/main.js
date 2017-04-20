045acba4954578bcef5179dc2273fb0d0bb9a7db
Felipe de Araujo <daraujofelipe@gmail.com>
2017-04-20 11:12:10 -0300

    Improves authentication state

--- app/main.js
+++ app/main.js
@@ -2,27 +2,60 @@
 
 angular.module('testaller.main', [])
 
+.config(['$stateProvider', function($stateProvider) {
+  $stateProvider.state('app', {
+    abstract: true,
+    url: '/app',
+    controller: 'MainCtrl',
+    template: '<ui-view/>'
+  });
+}])
+
 .controller('MainCtrl', ['$scope', '$window', 'authManager', '$localStorage',
-                        function($scope, $window, authManager, $localStorage) {
+                         '$http', 'API_URL', '$timeout',
+                         function($scope, $window, authManager, $localStorage,
+                         $http, API_URL, $timeout) {
 
-  $scope.status = {
-    isopen: false,
-    isAuthenticated: authManager.isAuthenticated()
-  };
-
+  $scope.error = {};
+  $scope.loading = false;
   $scope.isNavCollapsed = true;
 
-  $scope.toggleDropdown = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-    $scope.status.isopen = !$scope.status.isopen;
+  $scope.isAuthenticated = function() {
+    return authManager.isAuthenticated();
+  };
+
+  $scope.signin = function(email, password) {
+    $scope.loading = true;
+
+    $http({
+      url: API_URL + '/auth/signin',
+      skipAuthorization: true,
+      method: 'POST',
+      data: {
+        email: email,
+        password: password
+      }
+    }).then(function(response) {
+      if(response.data.auth_token) {
+        $localStorage.auth_token = response.data.auth_token;
+
+        // timeout a seguir evita que a aplicação tente navegar para home antes
+        // que angular-jwt faça a autenticação da aplicação
+        $timeout(function(){
+          $window.location.href = '/#!/app/home';
+        }, 1000);
+      } else {
+        $scope.error.message = response.message;
+        $scope.loading = false;
+      }
+    });
   };
 
   $scope.signout = function() {
-    authManager.unauthenticate();
+    delete $localStorage.auth_token;
 
-    $window.location.href = '#!/signin';
-    $window.location.reload();
+    $timeout(function(){
+      $window.location.href = '/#!/app/signin';
+    }, 1000);
   };
-
 }]);
